ee79c907724222525a0bd58a36a8cf5f4898eff1
Kiss Áron <aron123dev@gmail.com>
2025-01-03 21:10:43 +0100

    Fix possible NaN values in Supervised Contrastive Loss

--- core/loss/SupervisedContrastiveLoss.py
+++ core/loss/SupervisedContrastiveLoss.py
@@ -13,7 +13,6 @@ class SupConLoss(nn.Module):
 
     def forward(self, embeddings, labels):
         n = labels.shape[0]  # batch
-        # similarity_matrix = torch.matmul(features, features.T)
         similarity_matrix = F.cosine_similarity(embeddings.unsqueeze(1), embeddings.unsqueeze(0), dim=2)
 
         mask = torch.ones_like(similarity_matrix) * (labels.expand(n, n).eq(labels.expand(n, n).t()))
@@ -29,13 +28,11 @@ class SupConLoss(nn.Module):
 
         no_sim_sum = torch.sum(no_sim, dim=1)
         no_sim_sum_expend = no_sim_sum.repeat(n, 1).T
-        sim_sum = sim + no_sim_sum_expend
+        epsilon = 1e-8
+        sim_sum = sim + no_sim_sum_expend + epsilon
 
         loss_partial = -torch.log(mask_no_sim + torch.div(sim, sim_sum) + torch.eye(n, n).to(self.device))
 
         loss = torch.sum(torch.sum(loss_partial, dim=1)) / (len(torch.nonzero(loss_partial)))
 
         return loss
-
-
-
